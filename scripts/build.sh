7fa7292d2b635f8e1c975a2acc361af5fb671a01
yanhai.weng <yanhai.weng@lkcoffee.com>
2026-02-28 10:34:32 +0800

    11
    11

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
-sudo apt-get install -y build-essential gcc g++ wget make perl \
-  libpcre3-dev libssl-dev zlib1g-dev automake autoconf libtool
+dnf install -y gcc gcc-c++ make wget perl automake autoconf libtool \
+  pcre-devel openssl-devel zlib-devel
 
-sudo mkdir -p /opt/apiok/openresty /opt/apiok/apiok
+mkdir -p /opt/apiok/openresty /opt/apiok/apiok
 
 chmod +x scripts/*.sh
-sudo make build
+make build
 make deps
-sudo make SKIP_SYSTEM_BIN=1 install
+make SKIP_SYSTEM_BIN=1 install
 
 mkdir -p release
-sudo cp -r /opt/apiok/apiok /opt/apiok/openresty release/
+cp -r /opt/apiok/apiok /opt/apiok/openresty release/
