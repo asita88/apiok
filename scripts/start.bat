ab4bebbc92d06566c527aac9bcdd80f62f9747f5
asita88 <342122003@qq.com>
2026-05-10 22:29:44 +0800

    logs dir
    logs dir

--- scripts/start.bat
+++ scripts/start.bat
@@ -144,7 +144,7 @@ docker run -d ^
     -p 80:80 ^
     -p 443:443 ^
     -p 8080:8080 ^
-    -v "%cd%\logs:/opt/apiok/apiok/logs" ^
+    -v "%cd%\logs:/var/log/apiok" ^
     --restart unless-stopped ^
     %IMAGE_NAME%:latest
 
